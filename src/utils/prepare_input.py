24832d66b9745e9d8bf95edcde780b5ce18d64d4
camilojd <camilo.jara@ug.uchile.cl>
2020-08-06 11:22:50 -0400

    adding utils

---
+++ src/utils/prepare_input.py
@@ -0,0 +1,57 @@
+def prepare_ctrl_input(args, _, tokenizer, prompt_text):
+    if args.temperature > 0.7:
+        logger.info("CTRL typically works better with lower temperatures (and lower top_k).")
+
+    encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False)
+    if not any(encoded_prompt[0] == x for x in tokenizer.control_codes.values()):
+        logger.info("WARNING! You are not starting your generation from a control code so you won't get good results")
+    return prompt_text
+
+
+def prepare_xlm_input(args, model, tokenizer, prompt_text):
+    # kwargs = {"language": None, "mask_token_id": None}
+
+    # Set the language
+    use_lang_emb = hasattr(model.config, "use_lang_emb") and model.config.use_lang_emb
+    if hasattr(model.config, "lang2id") and use_lang_emb:
+        available_languages = model.config.lang2id.keys()
+        if args.xlm_language in available_languages:
+            language = args.xlm_language
+        else:
+            language = None
+            while language not in available_languages:
+                language = input("Using XLM. Select language in " + str(list(available_languages)) + " >>> ")
+
+        model.config.lang_id = model.config.lang2id[language]
+
+    return prompt_text
+
+
+def prepare_xlnet_input(args, _, tokenizer, prompt_text):
+    prompt_text = (args.padding_text if args.padding_text else PADDING_TEXT) + prompt_text
+    return prompt_text
+
+
+def prepare_transfoxl_input(args, _, tokenizer, prompt_text):
+    prompt_text = (args.padding_text if args.padding_text else PADDING_TEXT) + prompt_text
+    return prompt_text
+    
+
+def prepare_bert_input(args, _, tokenizer, prompt_text):
+    prompt_text = prompt_text + " " + tokenizer.mask_token
+    return tokenizer.decode(tokenizer.encode(prompt_text, add_special_tokens=True)) 
+
+def prepare_marian_input(args, _, tokenizer, prompt_text):
+    special_token = f">>{args.translate_to}<< "
+    prompt_text = special_token + prompt_text 
+    return [prompt_text]
+
+
+PREPROCESSING_FUNCTIONS = {
+    "ctrl": prepare_ctrl_input,
+    "xlm": prepare_xlm_input,
+    "xlnet": prepare_xlnet_input,
+    "transfo-xl": prepare_transfoxl_input,
+    "bert": prepare_bert_input,
+    "marian": prepare_marian_input
+}

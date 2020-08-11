b53a6c89ef0526daa1f9d757904f9697cd54f3e6
Estacion prueba <gerencia@lifeware.cl>
2020-08-11 12:09:27 -0400

    server 1

--- src/models/run_generation_server.py
+++ src/models/run_generation_server.py
@@ -1,4 +1,4 @@
-from .GenerativeModel import GenerativeModel
+""" from .GenerativeModel import GenerativeModel
 from ..utils import do_parse_args
 import socket
 
@@ -57,4 +57,42 @@ def server(model):
 if __name__ == "__main__":
     args = do_parse_args()
     model = GenerativeModel(args)
-    server(model)
+    server(model) """
+
+import socketserver
+#import socket
+import json
+
+response = {
+  "sentences": ["John ef ","ann ere ","ssdaa erer", "saa er er"],
+  "error": False
+}
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        while True:
+            # escucha longitud de mensaje
+            self.msgl = self.request.recv(1024).strip().decode("utf-8")
+            print("largo: ",self.msgl)
+            
+            # escucha mensaje
+            self.msg = json.loads(self.request.recv(int(self.msgl)).strip().decode("utf-8"))
+            print("{}:".format(self.client_address[0]))
+            print(self.msg)
+
+
+            # envia longitud de respuesta
+            l = len(json.dumps(response))
+            print("- l: ",bytes(str(l), 'utf-8'))
+            self.request.send(bytes(str(l), 'utf-8'))
+
+            # envia respuesta
+            self.request.sendall(bytes(json.dumps(response), 'utf-8'))
+
+if __name__ == "__main__":
+    HOST, PORT = "192.168.1.199", 11000
+    print(HOST)
+    print(PORT)
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server.serve_forever()

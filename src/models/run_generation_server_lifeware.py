5e4b9d73958d26a3bff864293dc560d25d735a49
Estacion prueba <gerencia@lifeware.cl>
2020-09-01 13:59:59 -0400

    Reporta a optikey que el servicio esta activo.

--- src/models/run_generation_server_lifeware.py
+++ src/models/run_generation_server_lifeware.py
@@ -1,5 +1,5 @@
 import socketserver
-#import socket
+import socket
 import json
 from .GenerativeModel import GenerativeModel
 from ..utils import do_parse_args
@@ -11,7 +11,7 @@ response = {
   "error": False
 }
 model_sentences = None
-model_words = None
+#model_words = None
 
 def mapping(language,context):
 
@@ -31,10 +31,18 @@ def set_parameters(args,modelType,modelNameOrPath,length,numReturnSequences):
 	return args
 
 def get_sentences(text):
-	return model_sentences.run(text)
+	if text[len(text)-1].isspace() == True:
+		print("ultimo elemento es espacio")
+		text = text[0:len(text)-1]
+	lgth = len(text)
+	predicted_tokens = model_sentences.run(text)
+	predicted = []
+	for predic in predicted_tokens:
+		predicted.append(predic[lgth:len(predic)])
+	return predicted
 
-def get_words(text):
-	return model_words.run(text)
+#def get_words(text):
+#	return model_words.run(text)
 
 def set_model(model,args):
 
@@ -44,6 +52,7 @@ def set_model(model,args):
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
 	def handle(self):
+
 		while True:
 			# escucha longitud de mensaje
 			self.msgl = self.request.recv(1024).strip().decode("utf-8")
@@ -53,9 +62,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 			self.msg = json.loads(self.request.recv(int(self.msgl)).strip().decode("utf-8"))
 			print("{}:".format(self.client_address[0]))
 			print(self.msg)
+			print(response)
 			if "text" in self.msg.keys():
 				response["sentences"] = get_sentences(self.msg["text"])
-				response["words"] = get_words(self.msg["text"])
+				#response["words"] = get_words(self.msg["text"])
 			else:
 				print("no text")
 
@@ -64,6 +74,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 			else:
 				print("no language and context")
 			# envia longitud de respuesta
+			print(response)
 			l = len(json.dumps(response))
 			print("- l: ",bytes(str(l), 'utf-8'))
 			self.request.send(bytes(str(l), 'utf-8'))
@@ -80,18 +91,18 @@ if __name__ == "__main__":
 	# print('length =', args.length)
 	# print('num_return_sequences =', args.num_return_sequences)
 	# print('no_cuda =', args.no_cuda)
-	
-	args_sentences = set_parameters(args,"gpt2","gpt2",5,2)
-	args_words = set_parameters(args,"gpt2","gpt2",4,1)
-	
-
-
-
-
+	args_sentences = set_parameters(args,"gpt2","gpt2",4,4)
+	args_words = set_parameters(args,"gpt2","gpt2",1,4)
 	model_sentences = GenerativeModel(args_sentences)
-	model_words = GenerativeModel(args_words)
+	#model_words = GenerativeModel(args_words)
 	HOST, PORT = "127.0.0.1", 11000
 	print(HOST)
 	print(PORT)
 	server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+	print("StartServer")
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.connect((HOST, 11001))
+			s.sendall(b'serverStart')
 	server.serve_forever()
+		
+

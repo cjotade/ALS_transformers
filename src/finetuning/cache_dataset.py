e28497634b26efb35d3b0b98bf6da19607624238
camilojd <camilo.jara@ug.uchile.cl>
2020-09-24 19:43:56 -0300

    last fix

--- src/finetuning/cache_dataset.py
+++ src/finetuning/cache_dataset.py
@@ -1,15 +1,61 @@
 import os
-import torch
+import argparse
+
 from transformers import BertTokenizerFast
 from .LineByLineNLPTextDataset import LineByLineNLPTextDataset
 
+def create_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name_or_path",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to pre-trained tokenizer or shortcut name", # "./weights/beto/"
+    )
+    parser.add_argument(
+        "--data_folder",
+        default=None,
+        type=str,
+        required=True,
+        help="Dataset folder path", # "./data/spanish-corpora"
+    )
+    parser.add_argument(
+        "--save_path",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to store cached dataset", # "./data/cached_datasets/spanish-corpora-cached"
+    )
+    parser.add_argument("--block_size", type=int, default=128)
+    parser.add_argument("--overwrite_cache", type=bool, default=False)
+    parser.add_argument("--data_types", type=str, default="txt")
 
-data_path = "./data/minicorpus"
-TRAIN_PATHS = [os.path.join(data_path, filename) for filename in filter(lambda x: x.endswith((".txt")), os.listdir(data_path))]
+    args = parser.parse_args()
+    return args
 
-block_size = 128
-tokenizer = BertTokenizerFast.from_pretrained("./weights/beto/", do_lower_case=False)
-dataset = LineByLineNLPTextDataset(tokenizer, TRAIN_PATHS, block_size, overwrite_cache=False)
 
-data_path = "./data/cached_datasets/spanish-corpora-cached"
-dataset.save_dataset_to_disk(data_path)
+def main():
+    args = create_args()
+
+    data_folder = args.data_folder 
+    block_size = args.block_size
+    save_path = args.save_path 
+
+    # Load txt files in data_folder
+    TRAIN_PATHS = [os.path.join(data_folder, filename) for filename in filter(lambda x: x.endswith((args.data_types)), os.listdir(data_folder))]
+    # Load tokenizer from model_name_or_path
+    tokenizer = BertTokenizerFast.from_pretrained(args.model_name_or_path, do_lower_case=False) 
+    # Caching dataset
+    dataset = LineByLineNLPTextDataset(tokenizer, TRAIN_PATHS, block_size, overwrite_cache=args.overwrite_cache)
+
+    # Store cached dataset
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    dataset.save_dataset_to_disk(save_path)
+
+if __name__ == "__main__":
+    main()
+
+
+    
--- src/finetuning/train_lstm_model.py
+++ src/finetuning/train_lstm_model.py
@@ -1,47 +1,96 @@
+import os
+import argparse
 
 from transformers.configuration_utils import PretrainedConfig
 from transformers import BertTokenizerFast
-from ..models import GenerativeLSTM
-from .LineByLineNLPTextDataset import LineByLineNLPTextDataset
-
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForLanguageModeling
 
-OUTPUT_DIR = f"./weights/es-lstm_test/"
-cached_dataset_path = "./data/cached_datasets/spanish-corpora-cached"
-
-block_size = 128
-
-config = PretrainedConfig()
-tokenizer = BertTokenizerFast.from_pretrained("./weights/beto/", do_lower_case=False)
-model = GenerativeLSTM(config)
+from .LineByLineNLPTextDataset import LineByLineNLPTextDataset
+from ..models import GenerativeLSTM
 
 
-dataset = LineByLineNLPTextDataset(tokenizer, cached_dataset_path, block_size, overwrite_cache=False)
+def create_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_dir",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to pre-trained tokenizer or shortcut name", # "./weights/es-lstm_test/"
+    )
+    parser.add_argument(
+        "--model_name_or_path",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to pre-trained tokenizer or shortcut name", # "./weights/beto/"
+    )
+    parser.add_argument(
+        "--cached_dataset_path",
+        default=None,
+        type=str,
+        required=True,
+        help="Dataset folder path", # "./data/cached_datasets/spanish-corpora-cached"
+    )
+    parser.add_argument("--block_size", type=int, default=128)
+    parser.add_argument("--overwrite_cache", type=bool, default=False)
+
+    parser.add_argument("--per_device_train_batch_size", type=int, default=32)
+    parser.add_argument("--save_steps", type=int, default=10_000)
+    parser.add_argument("--save_total_limit", type=int, default=2)
+    parser.add_argument("--warmup_steps", type=int, default=10_000)
+    parser.add_argument("--max_steps", type=int, default=90_000)
+    parser.add_argument("--learning_rate", type=float, default=0.0001)
+    parser.add_argument("--fp", type=bool, default=False)
+    parser.add_argument("--mlm", type=int, default=False)
+    parser.add_argument("--mlm_probability", type=float, default=0.15)
+
+    args = parser.parse_args()
+    return args
+
+def main():
+
+    args = create_args()
+
+    OUTPUT_DIR = args.output_dir 
+    cached_dataset_path = args.cached_dataset_path 
+
+    block_size = args.block_size
+
+    config = PretrainedConfig()
+    tokenizer = BertTokenizerFast.from_pretrained(args.model_name_or_path, do_lower_case=False) 
+    model = GenerativeLSTM(config)
 
 
-training_args = TrainingArguments(
-    output_dir=OUTPUT_DIR,
-    overwrite_output_dir=True,
-    per_device_train_batch_size=32,
-    save_steps=10_000,
-    save_total_limit=2,
-    warmup_steps=10_000,
-    max_steps=90_000,
-    learning_rate=0.0001,
-    fp16=True
-)
+    dataset = LineByLineNLPTextDataset(tokenizer, cached_dataset_path, block_size, overwrite_cache=args.overwrite_cache)
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False, mlm_probability=0.15
-)
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=dataset,
-    prediction_loss_only=True,
-)
+    training_args = TrainingArguments(
+        output_dir=OUTPUT_DIR,
+        overwrite_output_dir=True,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        save_steps=args.save_steps,
+        save_total_limit=args.save_total_limit,
+        warmup_steps=args.warmup_steps,
+        max_steps=args.max_steps,
+        learning_rate=args.learning_rate,
+        fp16=args.fp
+    )
 
-trainer.train()
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=args.mlm, mlm_probability=args.mlm_probability
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=dataset,
+        prediction_loss_only=True,
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    main()

d1b232e321e3a81c4232149dd9b025cb578ab43a
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2020-10-23 02:37:56 -0300

    Adding post-processing tools like cleaning special chars and parse contractions, also refactoring code

--- src/models/GenerativeModel.py
+++ src/models/GenerativeModel.py
@@ -1,3 +1,4 @@
+import contractions
 import numpy as np
 import torch
 
@@ -6,6 +7,8 @@ from lm_scorer.models.auto import AutoLMScorer as LMScorer
 from ..utils import get_tokenizer_and_model
 from ..utils import PREPROCESSING_FUNCTIONS
 from ..utils import do_parse_args
+
+SPECIAL_CHARS = '.,¡!¿?()/-_";|:।॥#~[]{}*¨@$%&^`¬·|'
 
 class GenerativeModel:
     def __init__(self, args, set_translators_and_scorer=True):
@@ -80,17 +83,31 @@ class GenerativeModel:
         return encoded_prompt, input_ids
         
     def generate_bert(self, input_ids, num_return_sequences):
+        print("HENERANDO EL MEO VBBEERRTTT")
+        print("input_ids", input_ids)
         hidden_reps = self.model(input_ids)[0]
+        print("hidden_reps", hidden_reps)
         tokenized_sentence = self.tokenizer.tokenize(self.tokenizer.decode(input_ids[0]))
+        print("tokenized_sentence", tokenized_sentence)
         mask_idx = np.where(np.array(tokenized_sentence) == self.tokenizer.mask_token)[0][0]
+        print("mask_idx", mask_idx)
         idxs = torch.argsort(hidden_reps[0, mask_idx], descending=True)
-        first_idxs = idxs[:num_return_sequences]
+        print("idxs", idxs)
+        first_idxs = []
+        for idx in idxs:
+            if len(first_idxs) == num_return_sequences:
+                break
+            if not self.tokenizer.decode([idx]).startswith(tuple(SPECIAL_CHARS+"'")):
+                first_idxs.append(idx)
+        print("first_idxs", first_idxs)
         output_sequences = []
         for idx in first_idxs:
             tokenized_sentence[mask_idx] = self.tokenizer.decode([idx])
-            sequence = self.tokenizer.encode(
+            print("tokenized_sentence", tokenized_sentence)
+            sequence_arr = self.tokenizer.encode(
                 tokenized_sentence[1:-1], add_special_tokens=False, add_space_before_punct_symbol=True)
-            output_sequences.append(sequence)
+            print("sequence_arr", sequence_arr)
+            output_sequences.append(sequence_arr)
         return np.array(output_sequences)
 
     def optimal_length(self, total_sequence, len_prompt):
@@ -110,10 +127,8 @@ class GenerativeModel:
         optimal_seq = length_options[optimal_sentence] 
         optimal_score = max(scores)
         return optimal_seq, optimal_score
-    
-    def generate_sentences(self, prompt_text, args):
-        encoded_prompt, input_ids = self.format_input(prompt_text, args)
-        
+
+    def generate_array_sequences(self, input_ids, encoded_prompt, args, num_return_sequences):
         if (args.model_type != "bert") and (args.model_type != "marian"):
             output_sequences = self.model.generate(
                 input_ids=input_ids,
@@ -123,33 +138,28 @@ class GenerativeModel:
                 top_p=args.p,
                 repetition_penalty=args.repetition_penalty,
                 do_sample=True,
-                num_return_sequences=args.num_return_sequences,
+                num_return_sequences=num_return_sequences,
                 sample=args.sample,
                 num_iterations=args.num_iterations,
-                #grad_length=args.grad_length,
-                #horizon_length=args.horizon_length,
-                #window_length=args.window_length,
-                #decay=args.decay,
-                #gamma=args.gamma,
-                #gm_scale=args.gm_scale,
-                #kl_scale=args.kl_scale,
             )
         elif (args.model_type == "marian"):
             output_sequences = self.model.generate(
                 input_ids=input_ids
             )
         else:
+            print("generating bert")
             output_sequences = self.generate_bert(
                 input_ids=input_ids,
-                num_return_sequences=args.num_return_sequences
+                num_return_sequences=num_return_sequences
             )
-
         # Remove the batch dimension when returning multiple sequences
         if len(output_sequences.shape) > 2:
             output_sequences.squeeze_()
-        
-        generated_sequences = []
+        return output_sequences
 
+    def transform_sequences(self, output_sequences, prompt_text, encoded_prompt, args):
+        generated_sequences = []
+        skipped_sequences = []
         for _, generated_sequence in enumerate(output_sequences):
             generated_sequence = generated_sequence.tolist()
 
@@ -176,33 +186,63 @@ class GenerativeModel:
                 total_sequence = text
 
             if (args.model_type != "marian"):
-                optimal_seq, optimal_score = self.optimal_length(total_sequence, len(encoded_prompt[0]))
-                print("model output:", total_sequence)
-                print("lm_scorer output:", optimal_seq)
+                optimal_seq, _ = self.optimal_length(total_sequence, len(encoded_prompt[0]))
+                print("Model Output:", total_sequence.replace("\n", ""))
+                print("Lm_scorer Output:", optimal_seq.replace("\n", ""))
             else:
                 optimal_seq = total_sequence
 
-            # Deleting prompt_text for return
-            optimal_seq = optimal_seq[len(prompt_text):].strip()
-            print("optimal deletion:", optimal_seq)
-            print()
-            generated_sequences.append(optimal_seq)
+            # Deleting prompt text at start and clean break lines
+            seq_deletion = optimal_seq[len(prompt_text):].strip()
+            seq_deletion = seq_deletion.replace("\n", "")
+            if (seq_deletion == "") or (seq_deletion.startswith(tuple(SPECIAL_CHARS))):
+                print("Seq deletion (skip):", seq_deletion)
+                skipped_sequences.append(seq_deletion)
+            else:
+                if seq_deletion.startswith("'"):
+                    seq_deletion = contractions.fix(prompt_text + seq_deletion)
+                    seq_deletion = seq_deletion[len(prompt_text):].strip()
+                    print("Seq deletion (contractions):", seq_deletion)
+                else:
+                    print("Seq deletion (normal):", seq_deletion)
+                generated_sequences.append(seq_deletion)
 
-        return generated_sequences
+        return generated_sequences, skipped_sequences
+
+    def generate_sentences(self, prompt_text, args):
+        encoded_prompt, input_ids = self.format_input(prompt_text, args)
+        # Generate array sequences
+        output_sequences = self.generate_array_sequences(input_ids, encoded_prompt, args, args.num_return_sequences)
+        # Generation
+        print("\nEncoding sequences...") 
+        generated_sequences, skipped_sequences = self.transform_sequences(output_sequences, prompt_text, encoded_prompt, args)
+        # Post-Processing
+        if skipped_sequences:   
+            print("\nPost-Processing skipped sequences...") 
+            output_sequences_skips = self.generate_array_sequences(input_ids, encoded_prompt, args, len(skipped_sequences))
+            generated_sequences_skips, skipped_sequences_skips  = self.transform_sequences(output_sequences_skips, prompt_text, encoded_prompt, args)
+            # Merge
+            sequences = generated_sequences + generated_sequences_skips + skipped_sequences + skipped_sequences_skips
+            sequences = sequences[:args.num_return_sequences]
+        else:
+            sequences = generated_sequences
+        return sequences
 
     def run(self, sentence):
         # Pre-processing
         sentence = sentence.strip()
         if self.translator_input is not None:
+            print("\nRunning traslator input...") 
             # Spanish to English
             translated_sequence = self.translator_input.generate_sentences(sentence, self.translator_input.args)
             sentence = translated_sequence[0]
-            print(f"Translator input: {sentence}")
+            print(f"\nTranslator input: {sentence}")
         # Generate sequences
+        print("\nGenerating sequences from:", sentence)
         generated_sequences = self.generate_sentences(sentence, self.args)
-        print(f"Generated: {generated_sequences}")
-        print()
+        print(f"\nGenerated sequences: {generated_sequences}")
         if self.translator_output is not None:
+            print("\nRunning traslator output...") 
             # English to Spanish
             translated_sequences = []
             for sentence in generated_sequences:
